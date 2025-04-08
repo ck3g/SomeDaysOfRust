5772494de7a996af0cccb7803e04e6f869d14ea9
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-04-08 18:32:31 +0200

    Options

--- 02-rustlings/rustlings/exercises/quizzes/quiz2.rs
+++ 02-rustlings/rustlings/exercises/quizzes/quiz2.rs
@@ -28,6 +28,19 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result: Vec<String> = vec![];
+
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => result.push(string.to_uppercase()),
+                Command::Trim => result.push(string.trim().to_string()),
+                Command::Append(n) => result.push(format!("{string}{}", "bar".repeat(n))),
+            }
+        }
+
+        result
+    }
 }
 
 fn main() {
@@ -38,6 +51,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
